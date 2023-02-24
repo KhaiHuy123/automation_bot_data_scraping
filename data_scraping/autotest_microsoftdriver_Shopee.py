f718773f75c605e41443a684ad12f6143a8dd62f
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2023-02-24 22:09:41 +0700

    Update autotest_microsoftdriver_Shopee.py
    new_version

--- data_scraping/autotest_microsoftdriver_Shopee.py
+++ data_scraping/autotest_microsoftdriver_Shopee.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 import pandas as pd
-import numpy as np
 import os
 from datetime import datetime
 import sys, time
